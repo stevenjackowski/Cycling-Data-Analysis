42081e06bd383e6026d7941812b4e67ec87c5eb1
Steven Jackowski <sjackowski@kpmg.com>
2018-07-12 18:18:32 -0700

    First commit

---
+++ env/lib/python3.6/site-packages/fitparse/__init__.py
@@ -0,0 +1,9 @@
+from fitparse.base import FitFile, FitParseError
+from fitparse.processors import FitFileDataProcessor, StandardUnitsDataProcessor
+
+
+__version__ = '1.0.1'
+__all__ = [
+    'FitFileDataProcessor', 'FitFile', 'FitParseError',
+    'StandardUnitsDataProcessor',
+]
